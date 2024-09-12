ea5623b01a90951ba5f1f815411c4ace92e14c6c
seeyous00n <borovikov.learn@gmail.com>
2024-09-12 21:35:45 +0300

    Isograms

---
+++ codewars/kyu 7/isograms.js
@@ -0,0 +1,31 @@
+// DESCRIPTION
+// Isograms
+// An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+//
+// Example: (Input --> Output)
+//
+// "Dermatoglyphics" --> true
+// "aba" --> false
+// "moOse" --> false (ignore letter case)
+//
+// SOLUTION
+function isIsogram(str) {
+  if (str === "") return true;
+
+  const result = str
+    .toLowerCase()
+    .split("")
+    .reduce((accum, value) => {
+      if (accum.hasOwnProperty(value)) {
+        accum[value] = accum[value] + 1;
+      } else {
+        accum[value] = 1;
+      }
+      return accum;
+    }, {});
+  return Math.max(...Object.values(result)) === 1;
+}
+
+console.log(isIsogram("Dermatoglyphics"));
+console.log(isIsogram("moOse"));
+console.log(isIsogram("isIsogram"));
