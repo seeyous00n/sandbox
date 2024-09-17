5dbcb7c09e455a58dc4ebf0db4ad3dbf20aa643a
seeyous00n <borovikov.learn@gmail.com>
2024-09-17 16:56:20 +0300

    CSV representation of array

---
+++ codewars/kyu 8/csv-representation-of-array.js
@@ -0,0 +1,30 @@
+// CSV representation of array
+//
+// Description:
+// Create a function that returns the CSV representation of a two-dimensional numeric array.
+//
+// Example:
+//
+// input:
+//    [[ 0, 1, 2, 3, 4 ],
+//     [ 10,11,12,13,14 ],
+//     [ 20,21,22,23,24 ],
+//     [ 30,31,32,33,34 ]]
+//
+// output:
+//      '0,1,2,3,4\n'
+//     +'10,11,12,13,14\n'
+//     +'20,21,22,23,24\n'
+//     +'30,31,32,33,34'
+
+// SOLUTION
+const toCsvText = (array) => {
+  return array.reduce((accum, curentValue, index, array) => {
+    if (!array[index + 1]) {
+      accum += curentValue.join(",");
+    } else {
+      accum += curentValue.join(",") + "\n";
+    }
+    return accum;
+  }, "");
+};
