537a4ef9fd4c2b1152de7bb9fb1241524feb07ca
seeyous00n <borovikov.learn@gmail.com>
2024-09-22 19:10:00 +0300

    Convert boolean values to strings 'Yes' or 'No'.

---
+++ codewars/kyu 8/convert-boolean-values-to-strings.sql
@@ -0,0 +1,7 @@
+-- # write your SQL statement here: you are given a table 'booltoword' with column 'bool', return a table with column 'bool' and your result in a column named 'res'.
+SELECT bool, 
+  CASE WHEN bool = true 
+  THEN 'Yes' 
+  ELSE 'No' 
+  END AS res 
+FROM booltoword
