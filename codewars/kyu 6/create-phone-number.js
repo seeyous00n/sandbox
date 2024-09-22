9c171d7a96b1c788b7aa2d1f3d9ec46829b51fe6
seeyous00n <borovikov.learn@gmail.com>
2024-09-22 15:08:12 +0300

    Create Phone Number

---
+++ codewars/kyu 6/create-phone-number.js
@@ -0,0 +1,18 @@
+// Create Phone Number
+//
+// Description:
+// Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+//
+// Example
+// createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+// The returned format must be correct in order to complete this challenge.
+
+// SOLUTION
+const createPhoneNumber = (numbers) =>
+  `(${numbers[0]}${numbers[1]}${numbers[2]}) ${numbers[3]}${numbers[4]}${numbers[5]}-${numbers[6]}${numbers[7]}${numbers[8]}${numbers[9]}`;
+
+// Good solution
+// const createPhoneNumber = (numbers) =>
+//   numbers.reduce((p, c) => p.replace("x", c), "(xxx) xxx-xxxx");
+
+// console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
